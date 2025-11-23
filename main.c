bbacb95778c3ba977f340ade19c97305f4782780
Maverick7706 <kunal.thakore@gmail.com>
2025-11-23 22:04:18 +0530

    Working on addNode

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
-static Node *head;
+static Node *head1;
+static Node *head2;
 
 int main(void)
 {
-    head = (Node *)malloc(sizeof(Node));
+    head1 = (Node *)malloc(sizeof(Node));
     createLinkedList();
-    printLinkedList(head);
+    printLinkedList(head1);
+    addNode(head2,0);
+    printLinkedList(head2);
     return 0;
 }
 
@@ -17,31 +20,58 @@ void createLinkedList(void)
     Node *node3 = (Node *)malloc(sizeof(Node));
     Node *node4 = (Node *)malloc(sizeof(Node));
 
-    head->next = node1;
+    head1->next = node1;
     node1->next = node2;
     node2->next = node3;
     node3->next = node4;
     node4->next = NULL;
 
-    head->data = 10;
+    head1->data = 10;
     node1->data = 20;
     node2->data = 30;
     node3->data = 40;
     node4->data = 50;
 }
 
+bool listIsEmpty(Node *list)
+{
+    bool retVal = false;
+    retVal = (list == NULL)?true:false;
+    return(retVal);
+}
+
 void printLinkedList(Node *list)
 {
     Node *temp;
     temp = list;
-    while(temp != NULL)
+    while(!listIsEmpty(temp))
     {
         printf("%d\n",temp->data);
         temp = temp->next;
     }
 }
 
-void addNode(Node *list)
+Node* addNode(Node *list,uint16_t pos)
 {
-    
+    Node *temp1;
+    if(listIsEmpty(list) && (pos == 0)) // List is empty and we want to add the head.
+    {
+        printf("List is empty. Adding head.\n");
+        list = (Node *)malloc(sizeof(Node));
+        return(list);
+    }
+    else
+    {
+        if(pos == 0) // List is not empty and we want to add at head.
+        {
+            temp1 = (Node *)malloc(sizeof(Node));
+            temp1->next = list;
+            list = temp1;
+            return(list);
+        }
+        else
+        {
+
+        }
+    }
 }
--- main.h
+++ main.h
@@ -5,16 +5,19 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stddef.h>
 
 typedef struct myNode
 {
-    struct myNode *next;
+    struct myNode *next; // 8 bytes
     int data; // 4 bytes
 }Node;
 
 
 void createLinkedList(void);
+bool listIsEmpty(Node *list);
 void printLinkedList(Node *list);
-void addNode(Node *list);
+Node* addNode(Node *list,uint16_t pos); // pos is nth position from head (0 is head).
 
 #endif
